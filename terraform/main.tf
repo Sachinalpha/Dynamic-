5288b57ba63cdcaa7fc159ccee54faf924b0e2f0
Sachinalpha <108449743+Sachinalpha@users.noreply.github.com>
2025-11-28 15:30:49 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 }
 
 module "keyvault" {
-  source               = "./keyvault_module"
+  source               = "./key_vault_module"
   keyvault_name        = var.keyvault_name
   resource_group_name  = var.resource_group_name
   location             = var.location
