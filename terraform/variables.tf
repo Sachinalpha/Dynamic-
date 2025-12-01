1ddc16082450837a53bd89733b99c596cb73b7c9
Sachinalpha <108449743+Sachinalpha@users.noreply.github.com>
2025-12-01 10:57:32 +0530

    Define types for variables in variables.tf
    Added type definitions for existing variables and introduced new variables for vnet and subnet.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,27 @@
-variable "subscription_id" {}
-variable "tenant_id" {}
-variable "resource_group_name" {}
-variable "keyvault_name" {}
-variable "location" {}
-variable "subnet_id" {}
+variable "keyvault_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "vnet_rg" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+}
