b3a4de580a7e6aafe57e6e6d012816bd9354d071
Sachinalpha <108449743+Sachinalpha@users.noreply.github.com>
2025-12-01 10:46:36 +0530

    Update main.tf

--- terraform/key_vault_module/main.tf
+++ terraform/key_vault_module/main.tf
@@ -1,13 +1,35 @@
+# --------------------------------------------
+# Look up VNet
+# --------------------------------------------
+data "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  resource_group_name = var.vnet_rg
+}
+
+# --------------------------------------------
+# Look up Subnet
+# --------------------------------------------
+data "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  resource_group_name  = var.vnet_rg
+}
+
+# --------------------------------------------
+# Key Vault
+# --------------------------------------------
 resource "azurerm_key_vault" "kv" {
-  name                        = var.keyvault_name
-  location                    = var.location
-  resource_group_name         = var.resource_group_name
-  tenant_id                   = var.tenant_id
-  sku_name                    = "premium"
+  name                = var.keyvault_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tenant_id           = var.tenant_id
+  sku_name            = "premium"
 
   network_acls {
     default_action             = "Deny"
-    bypass                     = "AzureServices"       # Required
-    virtual_network_subnet_ids = [var.subnet_id]
+    bypass                     = "AzureServices"
+    virtual_network_subnet_ids = [data.azurerm_subnet.subnet.id]
   }
+}
+
 }
