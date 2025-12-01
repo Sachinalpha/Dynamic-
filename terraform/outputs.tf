8ed947cd32dd310cd5a5cf9d19e3d36e77d14cb4
Sachinalpha <108449743+Sachinalpha@users.noreply.github.com>
2025-12-01 11:25:53 +0530

    Update outputs to use module references for keyvault

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "keyvault_id" {
-  value = azurerm_key_vault.kv.id
+  value = module.keyvault.key_vault_id
 }
 
 output "key_vault_uri" {
-  value = azurerm_key_vault.kv.vault_uri
+  value = module.keyvault.key_vault_uri
 }
 
 output "subnet_id_used" {
-  value = data.azurerm_subnet.subnet.id
+  value = module.keyvault.subnet_id_used
 }
