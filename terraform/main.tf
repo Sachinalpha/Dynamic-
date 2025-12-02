6f5b8b0a608649a53d5a7cf287ac9652157b1a85
Sachinalpha <108449743+Sachinalpha@users.noreply.github.com>
2025-12-02 10:29:19 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,8 +7,7 @@ provider "azurerm" {
 module "keyvault" {
   source               = "./key_vault_module"
   keyvault_name        = var.keyvault_name
-  resource_group_name  = var.resource_group_name
-  location             = var.location
+  resource_group_name  = var.resource_group_name     #location             = var.location
   tenant_id            = var.tenant_id
   vnet_name           = var.vnet_name
   vnet_rg             = var.vnet_rg
