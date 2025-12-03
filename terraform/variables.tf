4980fac8cae4170fe3f52cb9142821a6dc6d192d
Sachinalpha <108449743+Sachinalpha@users.noreply.github.com>
2025-12-03 10:46:52 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "subscription_id" {}
 variable "tenant_id" {}
 variable "resource_group_name" {}
-variable "client_id"
+variable "client_id" {}
 variable "client_secret" {}
 variable "vnet_name" {}
 variable "vnet_rg" {}
