1aa74b3bb8357a33059b8621625a5ad93e704f30
Sachinalpha <108449743+Sachinalpha@users.noreply.github.com>
2025-12-03 10:43:31 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,8 @@
 variable "subscription_id" {}
 variable "tenant_id" {}
 variable "resource_group_name" {}
-#variable "keyvault_name" {}
+variable "client_id"
+variable "client_secret" {}
 variable "vnet_name" {}
 variable "vnet_rg" {}
 variable "subnet_name" {}
